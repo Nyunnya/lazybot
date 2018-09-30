2112447d34cdc301c7f23c5d6521c5ce286c3db6
Alyanah <40045317+alyanah@users.noreply.github.com>
2018-09-29 23:30:56 -0400

    Update type definitions

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,20 +1,37 @@
 declare module 'lazybot' {
     import * as Discord from 'discord.js';
 
+    export * from 'discord.js';
+
     type Command = {
         name: string;
         alias?: string;
-        aliases?: Array;
-        callback: Function;
+        aliases?: string[];
+        callback: CommandCallback;
     };
 
-    export * from 'discord.js';
+    type CommandCallback = (params: CommandCallbackParams) => void;
+
+    type CommandCallbackParams = {
+        client: Client;
+        message: Discord.Message;
+        displayName: string;
+        args: Argument[];
+        data: { [key: string]: any };
+    };
+
+    class Argument extends String {
+        public readonly member?: Discord.GuildMember;
+        public readonly channel?: Discord.TextChannel;
+
+        public readonly rawValue(): string;
+    }
 
     class Commands {
         public prefix?: string;
         public data?: object;
 
-        public hook(command: string, data: string | Function | Command): void;
+        public hook(command: string, data: string | CommandCallback | Command): void;
         public parse(message: Discord.Message, line?: string): void;
         public unhook(command: string): void;
         public load(source: string): void;
